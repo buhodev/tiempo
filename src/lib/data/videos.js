a96d97367ec7b3004b2f0ed26ef49968dc53dbff
Carlos Ivanchuk <buhodev@proton.me>
2022-05-20 23:20:10 -0400

    add video sources

---
+++ src/lib/data/videos.js
@@ -0,0 +1,112 @@
+export const VIDEOS = {
+	china: {
+		day: 'China-day.mp4',
+		sunset: 'China-sunset.mp4',
+		night: 'China-night-s.mp4'
+	},
+	city: {
+		day: 'City-day.mp4',
+		sunset: 'City-sunset-9.mp4',
+		night: 'City-night-c.mp4'
+	},
+	desert: {
+		day: 'Desert-day-T.mp4',
+		sunset: 'Desert-sunset-B.mp4',
+		night: 'Desert-night-6.mp4'
+	},
+	egypt: {
+		day: 'Egypt-day-D.mp4',
+		sunset: 'Egypt-sunset-n.mp4',
+		night: 'Egypt-night-0.mp4'
+	},
+	forest: {
+		day: 'Forest-day-Q.mp4',
+		sunset: 'Forest-sunset-4.mp4',
+		night: 'Forest-night-A.mp4'
+	},
+	france: {
+		day: 'France-day.mp4',
+		sunset: 'France-sunset-i.mp4',
+		night: 'France-night-4.mp4'
+	},
+	hillsAndMountains: {
+		day: 'HillsandMountains-day.mp4',
+		sunset: 'HillsandMountains-sunset.mp4',
+		night: 'HillsandMountains-night.mp4'
+	},
+	india: {
+		day: 'India-day.mp4',
+		sunset: 'India-sunset.mp4',
+		night: 'India-night.mp4'
+	},
+	indonesia: {
+		day: 'Indonesia-day-l.mp4',
+		sunset: 'Indonesia-sunset-j.mp4',
+		night: 'Indonesia-night.mp4'
+	},
+	italy: {
+		day: 'Italy-day.mp4',
+		sunset: 'Italy-sunset-r.mp4',
+		night: 'Italy-night.mp4'
+	},
+	itsukishimaGateJapan: {
+		day: 'ItsukishimaGateJapan-day.mp4',
+		sunset: 'ItsukishimaGateJapan-sunset.mp4',
+		night: 'ItsukishimaGateJapan-night.mp4'
+	},
+	japanMtFuji: {
+		day: 'Lake-day-J.mp4',
+		sunset: 'Lake-sunset-L.mp4',
+		night: 'Lake-night-N.mp4'
+	},
+	lake: {
+		day: 'London-day.mp4',
+		sunset: 'London-sunset.mp4',
+		night: 'London-night.mp4'
+	},
+	london: {
+		day: 'Mt.FujiJapan-day.mp4',
+		sunset: 'Mt.FujiJapan-sunset.mp4',
+		night: 'Mt.FujiJapan-night.mp4'
+	},
+	philippines: {
+		day: 'Philippines-day.mp4',
+		sunset: 'Philippines-sunset.mp4',
+		night: 'Philippines-night.mp4'
+	},
+	seaTheLighthouse: {
+		day: 'SeaLighthouse-day.mp4',
+		sunset: 'SeaLighthouse-sunset.mp4',
+		night: 'SeaLighthouse-night.mp4'
+	},
+	seaTheShip: {
+		day: 'SeaShip-day.mp4',
+		sunset: 'SeaShip-sunset.mp4',
+		night: 'SeaShip-night.mp4'
+	},
+	snow: {
+		day: 'snow-day.mp4',
+		sunset: 'snow-sunset-2.mp4',
+		night: 'snow-night.mp4'
+	},
+	street: {
+		day: 'Street-day-X.mp4',
+		sunset: 'Street-sunset-z.mp4',
+		night: 'Street-night.mp4'
+	},
+	tropical: {
+		day: 'Tropical-day.mp4',
+		sunset: 'Tropical-sunset.mp4',
+		night: 'Tropical-night.mp4'
+	},
+	usa: {
+		day: 'USA-day.mp4',
+		sunset: 'USA-sunset-L.mp4',
+		night: 'USA-night.mp4'
+	},
+	valley: {
+		day: 'Valley-day-H.mp4',
+		sunset: 'Valley-sunset.mp4',
+		night: 'Valley-night-B.mp4'
+	}
+};
