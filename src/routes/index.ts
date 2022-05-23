b22fdcfc302fab06cda3909eb747cf5a7e41dbd3
Carlos Ivanchuk <buhodev@proton.me>
2022-05-23 16:13:19 -0400

    feat: add page endpoint to fetch weather

---
+++ src/routes/index.ts
@@ -0,0 +1,35 @@
+import type { RequestEvent } from '@sveltejs/kit';
+
+const FETCH_OPTIONS = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+		'X-RapidAPI-Key': '71e59e906dmshfdc9ed284df2469p1b599cjsn0355f0f200d5'
+	}
+};
+
+/** @type {import('./__types/[id]').RequestHandler} */
+export async function get(event: RequestEvent) {
+	const { searchParams } = event.url;
+	const query = searchParams.get('q') ?? 'Barcelona';
+	const lang = searchParams.get('lang') ?? 'es';
+
+	const response = await fetch(
+		`https://weatherapi-com.p.rapidapi.com/current.json?q=${query}&lang=${lang}&days=3`,
+		FETCH_OPTIONS
+	);
+
+	const data = await response.json();
+
+	const { location, current } = data;
+
+	const body = {
+		location,
+		current
+	};
+
+	return {
+		status: 200,
+		body
+	};
+}
