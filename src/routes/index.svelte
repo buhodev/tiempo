3dbd9995bd620f10c189930fedbd2584cf7d601a
Carlos Ivanchuk <buhodev@proton.me>
2022-05-23 13:51:59 -0400

    feat: add title and description to index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>RapidAPI Weather</title>
+	<meta name="description" content="Get the weather forecast in a beautiful and performant app. View current and forecasted weather; and also precipitation, air quality, and temperature maps to see different conditions near you." />
+</svelte:head>
