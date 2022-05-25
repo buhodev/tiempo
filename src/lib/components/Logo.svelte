3d90229f0d4d5f135901b0e03c9d584fb6de9182
Carlos Ivanchuk <buhodev@proton.me>
2022-05-24 21:26:53 -0400

    fix: add margin between logo and text

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -1,5 +1,5 @@
 <a href="/" class="flex items-center">
-	<img src="/logo.svg" class="h-6" alt="" />
+	<img src="/logo.svg" class="h-6 mr-3" alt="" />
 	<span class="self-center text-lg sm:text-xl font-semibold whitespace-nowrap dark:text-white"
 		>Rapid<span class="font-normal">API</span> Weather</span
 	>
