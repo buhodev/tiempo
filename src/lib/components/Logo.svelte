37cd8191ff2b1f00333855de6c0ae5d6110db1e3
Carlos Ivanchuk <buhodev@proton.me>
2022-05-24 21:00:22 -0400

    feat: add logo component

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,6 @@
+<a href="/" class="flex items-center">
+	<img src="/logo.svg" class="h-6" alt="" />
+	<span class="self-center text-lg sm:text-xl font-semibold whitespace-nowrap dark:text-white"
+		>Rapid<span class="font-normal">API</span> Weather</span
+	>
+</a>
