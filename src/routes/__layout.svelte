df9afbab07ff4ea4a3cea37e4ecf8391f6c8ce54
Carlos Ivanchuk <buhodev@proton.me>
2022-05-25 01:01:32 -0400

    feat: add sidebar on desktop

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import '../app.css';
 	import Overlay from '$lib/components/Overlay.svelte';
+	import NavContent from '$lib/components/NavContent.svelte';
 </script>
 
 <!-- Mobile -->
@@ -10,9 +11,11 @@
 
 <!-- Desktop -->
 <div class="hidden md:flex h-screen">
-	<div class="overflow-y-auto dark:bg-gray-800">
-		<!-- Sidebar -->
-	</div>
+	<aside
+		class="hidden lg:block py-4 px-3 dark:bg-gray-800 w-72 overflow-y-auto h-screen border-r border-gray-200 dark:border-gray-700"
+	>
+		<NavContent/>
+	</aside>
 
 	<slot />
 </div>
