32c246f6dd2ac8a2c4a684087b5a3e9b69c9ecda
Carlos Ivanchuk <buhodev@proton.me>
2022-05-25 19:09:16 -0400

    feat: add endpoint test

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
+test('index endpoint can retrieve city by query params', async ({ page }) => {
+	await page.goto('/?q=tokyo');
+	expect(await page.textContent('h1')).toBe('Tokyo');
 });
