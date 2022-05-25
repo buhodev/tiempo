16e5b3afcdf4e68d2f1626569db66f7adf784d0c
Carlos Ivanchuk <buhodev@proton.me>
2022-05-24 22:26:59 -0400

    feat: add overlay component to layout page

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,20 @@
 <script>
 	import '../app.css';
+	import Overlay from '$lib/components/Overlay.svelte';
 </script>
 
-<div>
+<!-- Mobile -->
+<div class="flex md:hidden">
 	<slot />
 </div>
+
+<!-- Desktop -->
+<div class="hidden md:flex h-screen">
+	<div class="overflow-y-auto dark:bg-gray-800">
+		<!-- Sidebar -->
+	</div>
+
+	<slot />
+</div>
+
+<Overlay />
