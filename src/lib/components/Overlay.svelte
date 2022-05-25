e127cea6bfd31e117e40235b230af9fe27a7f8e3
Carlos Ivanchuk <buhodev@proton.me>
2022-05-25 00:30:30 -0400

    feat: add transitions

--- src/lib/components/Overlay.svelte
+++ src/lib/components/Overlay.svelte
@@ -2,19 +2,25 @@
 	import { page } from '$app/stores';
 	import { overlay } from '$lib/stores';
 	import Logo from './Logo.svelte';
+	import { fly, fade } from 'svelte/transition';
+	import { cubicInOut } from 'svelte/easing';
 </script>
 
 {#if $overlay}
 	<div class="fixed z-50 inset-0 overflow-y-auto lg:hidden">
 		<div
+			transition:fade={{ duration: 300 }}
 			on:click={overlay.close}
 			class="fixed inset-0 bg-black/20 backdrop-blur-sm dark:bg-slate-900/80"
 			aria-hidden="true"
 		/>
 		<div
+			in:fly={{ x: -300, duration: 300, easing: cubicInOut, opacity: 1 }}
+			out:fly={{ x: -300, duration: 300, easing: cubicInOut, opacity: 1 }}
 			class="relative h-full bg-white w-72 sm:w-80 p-6 dark:bg-slate-800"
 		>
 			<button
+				transition:fade
 				on:click={overlay.close}
 				type="button"
 				class="absolute z-10 top-4 right-4 w-8 h-8 flex items-center justify-center text-slate-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300"
