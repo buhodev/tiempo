0785d8929c1a28d86b802c9aa5e019940b8eeb34
Carlos Ivanchuk <buhodev@proton.me>
2022-05-25 14:31:36 -0400

    feat: add search component

---
+++ src/lib/components/Search.svelte
@@ -0,0 +1,21 @@
+<button
+	type="button"
+	class="hidden w-full items-center whitespace-nowrap rounded-md bg-white py-1.5 pl-2 pr-3 text-sm leading-6 text-slate-400 shadow-sm ring-1 ring-slate-900/10 hover:ring-slate-300 dark:bg-slate-800 dark:hover:bg-slate-700 lg:flex"
+	><svg width="24" height="24" fill="none" aria-hidden="true" class="mr-3 flex-none"
+		><path
+			d="m19 19-3.5-3.5"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		/><circle
+			cx="11"
+			cy="11"
+			r="6"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		/></svg
+	>Quick search...<span class="ml-auto flex-none pl-3 text-xs font-semibold">Ctrl K</span></button
+>
