c6021a77e41e2b9c3b27290080b7ad4eaaef975c
Carlos Ivanchuk <buhodev@proton.me>
2022-05-25 20:53:06 -0400

    feat: add sidebar and wrap main content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,6 +12,16 @@
 	/>
 </svelte:head>
 
-<h1 class="text-lg">{location.name}</h1>
-<div class="text-7xl font-bold">{current.temp_c}°</div>
-<p>{current.condition.text}</p>
+<div class="flex min-w-0 flex-1 flex-shrink flex-col dark:bg-gray-800">
+	<div class="no-scrollbar mt-8 flex-1 space-y-4 overflow-y-auto px-8 dark:text-white">
+		<h1 class="text-lg">{location.name}</h1>
+		<div class="text-7xl font-bold">{current.temp_c}°</div>
+		<p>{current.condition.text}</p>
+	</div>
+</div>
+
+<aside
+	class="hidden h-screen w-80 overflow-y-auto border-l border-gray-200 py-4 px-3 dark:border-gray-700 dark:bg-gray-800 md:block"
+>
+	<!-- Forecast! -->
+</aside>
