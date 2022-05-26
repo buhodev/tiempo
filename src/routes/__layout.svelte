121c4b4413fa768f9ca40b5d8713c3924722961d
Carlos Ivanchuk <buhodev@proton.me>
2022-05-25 21:32:17 -0400

    refactor: combine mobile & desktop layouts - remove second <slot/> that might be rendering the content again

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,13 +4,7 @@
 	import NavContent from '$lib/components/NavContent.svelte';
 </script>
 
-<!-- Mobile -->
-<div class="flex md:hidden">
-	<slot />
-</div>
-
-<!-- Desktop -->
-<div class="hidden h-screen md:flex">
+<div class="h-screen flex">
 	<aside
 		class="hidden h-screen w-72 overflow-y-auto border-r border-gray-200 py-4 px-3 dark:border-gray-700 dark:bg-gray-800 lg:block"
 	>
