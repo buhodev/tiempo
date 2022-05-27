2bdb2e9c0f71295ab22e5435de8fd190b8cd7323
Carlos Ivanchuk <buhodev@proton.me>
2022-05-26 22:52:36 -0400

    chore: format

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,7 +4,7 @@
 	import NavContent from '$lib/components/NavContent.svelte';
 </script>
 
-<div class="h-screen flex">
+<div class="flex h-screen">
 	<aside
 		class="hidden h-screen w-72 overflow-y-auto border-r border-slate-200 py-4 px-3 dark:border-slate-700 dark:bg-slate-900 lg:block"
 	>
