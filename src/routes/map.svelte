8c127f649c9fee847f16ca067507551f56a2a465
Carlos Ivanchuk <buhodev@proton.me>
2022-05-28 16:31:22 -0400

    feat: add map to map page

--- src/routes/map.svelte
+++ src/routes/map.svelte
@@ -1,6 +1,33 @@
+<script>
+	import * as L from 'leaflet';
+	import 'leaflet/dist/leaflet.css';
+	let map;
+
+	function createMap(container) {
+		let m = L.map(container).setView([51.505, -0.09], 13);
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution:
+				'© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(m);
+
+		return m;
+	}
+
+	function mapAction(container) {
+		map = createMap(container);
+		return {
+			destroy: () => {
+				map.remove();
+			}
+		};
+	}
+</script>
+
 <svelte:head>
 	<title>Map - RapidAPI Weather</title>
 	<meta name="description" content="" />
 </svelte:head>
 
-<h1 class="text-2xl font-bold">Map</h1>
+<div class="flex min-w-0 flex-1 flex-shrink flex-col dark:bg-slate-900">
+	<div class="flex h-screen w-full" use:mapAction />
+</div>
