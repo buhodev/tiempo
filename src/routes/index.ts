aa80729fb56e2ee787609cc196f471c4e6f19b25
Carlos Ivanchuk <buhodev@proton.me>
2022-05-30 13:19:49 -0400

    refactor: extract env logic

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,24 +1,13 @@
 import type { RequestEvent } from '@sveltejs/kit';
 import type { Location, Current } from '$lib/types';
 
-let RAPIAPI_HOST;
-let RAPIAPI_KEY;
-
-if (process.env.NODE_ENV === 'production') {
-    // For production
-    RAPIAPI_HOST = process.env.RAPIDAPI_HOST;
-    RAPIAPI_KEY = process.env.RAPIDAPI_KEY;
-} else {
-    // For development
-    RAPIAPI_HOST = import.meta.env.VITE_RAPIDAPI_HOST
-    RAPIAPI_KEY = import.meta.env.VITE_RAPIDAPI_KEY
-}
+import { RAPIDAPI_HOST, RAPIDAPI_KEY } from '$lib/data/env';
 
 const FETCH_OPTIONS = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Host': RAPIAPI_HOST,
-		'X-RapidAPI-Key': RAPIAPI_KEY
+		'X-RapidAPI-Host': RAPIDAPI_HOST,
+		'X-RapidAPI-Key': RAPIDAPI_KEY
 	}
 };
 
