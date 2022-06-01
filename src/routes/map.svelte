ac37a0cbd11b8f674ed70238c2aa5503d31e16f9
Carlos Ivanchuk <buhodev@proton.me>
2022-06-01 14:40:07 -0400

    refactor: add map on onMount

--- src/routes/map.svelte
+++ src/routes/map.svelte
@@ -26,40 +26,40 @@
 </script>
 
 <script lang="ts">
-	import * as L from 'leaflet';
+	import type { Location, Current } from '$lib/types';
 	import 'leaflet/dist/leaflet.css';
 	import Header from '$lib/components/Header.svelte';
+	import { onMount } from 'svelte';
 
-	export let location = { name: 'Tokyo', country: 'Japan', lat: 35.652832, lon: 139.839478 };
+	export let current: Current, location: Location;
 
-	let map;
+	let myMap: HTMLElement;
 
-	function createMap(container: HTMLElement) {
-		let m = L.map(container).setView([location.lat, location.lon], 5);
-		// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		// 	attribution:
-		// 		'© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-		// }).addTo(m);
+	let createMap = (container, location) => {}
+ 
+	onMount(async () => {
+		const { map, tileLayer } = await import('leaflet');
 
-		L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
-			attribution:
-				'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-			subdomains: 'abcd',
-			minZoom: 1,
-			maxZoom: 16
-		}).addTo(m);
+		createMap = (container: HTMLElement, location) => {
+			let m = map(container).setView([location.lat, location.lon], 5);
+			// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			// 	attribution:
+			// 		'© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+			// }).addTo(m);
+	
+			tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
+				attribution:
+					'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+				subdomains: 'abcd',
+				minZoom: 1,
+				maxZoom: 16
+			}).addTo(m);
+	
+			return m;
+		}
 
-		return m;
-	}
-
-	function insertMap(container: HTMLElement) {
-		map = createMap(container);
-		return {
-			destroy: () => {
-				map.remove();
-			}
-		};
-	}
+		createMap(myMap, location)
+	});	
 </script>
 
 <svelte:head>
@@ -71,5 +71,6 @@
 	<div class="flex items-center border-b border-slate-900/10 dark:border-slate-600 md:border-0">
 		<Header title={`${location.name}, ${location.country}`} />
 	</div>
-	<div class="flex h-screen w-full" use:insertMap />
+	
+	<div class="flex h-screen w-full" bind:this={myMap} />
 </div>
