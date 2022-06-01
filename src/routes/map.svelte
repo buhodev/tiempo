dc1502baeec28d2c71d7efcce6a4e874963413cb
Carlos Ivanchuk <buhodev@proton.me>
2022-06-01 06:59:06 -0400

    chore: add types and format

--- src/routes/map.svelte
+++ src/routes/map.svelte
@@ -1,32 +1,31 @@
-<script>
+<script lang="ts">
 	import * as L from 'leaflet';
 	import 'leaflet/dist/leaflet.css';
 	import Header from '$lib/components/Header.svelte';
 
-	export let location = {name: "Tokyo", country: "Japan", lat: 35.652832, lon: 139.839478}
+	export let location = { name: 'Tokyo', country: 'Japan', lat: 35.652832, lon: 139.839478 };
 
 	let map;
 
-	function createMap(container) {
+	function createMap(container: HTMLElement) {
 		let m = L.map(container).setView([location.lat, location.lon], 5);
 		// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		// 	attribution:
 		// 		'© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 		// }).addTo(m);
 
-		L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+		L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
 			attribution:
 				'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 			subdomains: 'abcd',
 			minZoom: 1,
-			maxZoom: 16,
-			ext: 'jpg'
+			maxZoom: 16
 		}).addTo(m);
 
 		return m;
 	}
 
-	function insertMap(container) {
+	function insertMap(container: HTMLElement) {
 		map = createMap(container);
 		return {
 			destroy: () => {
