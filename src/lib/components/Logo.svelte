a8d3be7653011a285e3d03fb71079bffbd75ccfd
Carlos Ivanchuk <buhodev@proton.me>
2022-06-10 22:11:57 -0400

    fix: update app name

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -1,6 +1,6 @@
 <a href="/" class="flex items-center">
 	<img src="/logo.svg" class="mr-3 h-6" alt="" />
 	<span class="self-center whitespace-nowrap text-lg font-semibold dark:text-white sm:text-xl"
-		>Rapid<span class="font-normal">API</span> Weather</span
+		>Tiempo</span
 	>
 </a>
