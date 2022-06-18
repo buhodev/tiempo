825a33e623fd8104380df9a924181622e48a6bd4
Carlos Ivanchuk <buhodev@proton.me>
2022-06-17 21:08:06 -0400

    feat: add addToast & dimissToast methods

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -15,3 +15,23 @@ export const overlay = createToggle();
 export const settings = writable({ tempUnit: 'temp_c', windVelUnit: 'wind_kph' });
 
 export const toasts = writable([]);
+
+export const addToast = (toast) => {
+	const id = Math.floor(Math.random() * 10000);
+
+	const defaults = {
+		id,
+		type: 'info',
+		dismissible: false,
+		timeout: 3000,
+		message: 'Hello!'
+	};
+
+	toasts.update((all) => [{ ...defaults, ...toast }, ...all]);
+
+	if (toast.timeout) setTimeout(() => dismissToast(id), toast.timeout);
+};
+
+export const dismissToast = (id: ToastOptions['id']) => {
+	toasts.update((all) => all.filter((t) => t.id !== id));
+};
