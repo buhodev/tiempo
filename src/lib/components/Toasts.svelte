3379e9dd1a8efb987a0f8fe2968aafdcc76b2aff
Carlos Ivanchuk <buhodev@proton.me>
2022-06-19 22:26:20 -0400

    feat: add Toasts cmponent

---
+++ src/lib/components/Toasts.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Toast from '$lib/components/Toast.svelte';
+	import { dismissToast, toasts } from '$lib/stores';
+</script>
+
+{#if $toasts}
+	<section
+		class="fixed top-0 left-0 right-0 w-full pointer-events-none flex flex-col mt-4 justify-center z-[1000]"
+	>
+		{#each $toasts as toast (toast.id)}
+			<Toast type={toast.type} on:dismiss={() => dismissToast(toast.id)}>
+				{toast.message}
+			</Toast>
+		{/each}
+	</section>
+{/if}
