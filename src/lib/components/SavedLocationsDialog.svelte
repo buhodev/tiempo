261e785dbd04171bc64c573987937871edb79245
Carlos Ivanchuk <buhodev@proton.me>
2022-06-30 22:24:51 -0400

    feat: go to selected location on click

--- src/lib/components/SavedLocationsDialog.svelte
+++ src/lib/components/SavedLocationsDialog.svelte
@@ -13,10 +13,14 @@
 		<ul class="mt-4 space-y-4 border-t border-slate-200 pt-6 dark:border-slate-700">
 			{#each $savedLocations as { name, id } (id)}
 				<li class="flex items-center justify-between">
-					<a href="#" class="font-medium leading-5">{name}</a>
+					<a
+						href="/?q={name}"
+						class="flex w-full items-center rounded-lg px-2 py-1.5 text-base font-medium leading-5 text-gray-900 hover:bg-slate-100 dark:text-white dark:hover:bg-slate-700"
+						>{name}</a
+					>
 					<button
 						on:click={() => deleteLocation(name)}
-						class="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-2 py-1 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 dark:bg-red-600 dark:text-red-100 dark:focus-visible:ring-offset-slate-800"
+						class="mx-2 inline-flex justify-center rounded-md border border-transparent bg-red-300 px-2 py-1 text-sm font-medium text-red-900 hover:bg-red-400 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 dark:bg-red-600 dark:text-red-100 dark:focus-visible:ring-offset-slate-800"
 						>delete</button
 					>
 				</li>
