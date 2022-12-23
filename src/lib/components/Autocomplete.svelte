842d996127355dbd97f8a733dddbc9608157f825
Carlos Ivanchuk <buhodev@proton.me>
2022-12-23 18:03:13 -0400

    refactor: rename $app/env to $app/environment

--- src/lib/components/Autocomplete.svelte
+++ src/lib/components/Autocomplete.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { browser } from '$app/env';
+	import { browser } from '$app/environment';
 	import { goto } from '$app/navigation';
 	import { createEventDispatcher } from 'svelte';
 	import { fade } from 'svelte/transition';
